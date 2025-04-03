18a08ff618c50d6438f396d873b23341da344002
biehay <bhaymesbiedma@udesa.edu.ar>
2025-04-03 17:29:34 -0300

    avance con aparicion de nombre en headerloguado y profile.ejs

--- miApp/controllers/loginController.js
+++ miApp/controllers/loginController.js
@@ -2,6 +2,10 @@ const controlador = {
     mostrarLogin: function(req,res){
         return res.render('login')
     },
+    procesoLogin: function (req, res) {
+        let userName = req.body.userName;
+        res.render('profile', {userName: userName });
+      }
 };
 
 module.exports = controlador;
--- miApp/routes/login.js
+++ miApp/routes/login.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.get('/', loginController.mostrarLogin)
+router.get('/procesoLogin', loginController.procesoLogin)
 
 module.exports = router;
