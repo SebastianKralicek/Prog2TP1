ad44c140eebe0ee0fde497d26803a213e5d5c33d
biehay <bhaymesbiedma@udesa.edu.ar>
2025-04-03 18:02:00 -0300

    .

--- miApp/controllers/productAddController.js
+++
@@ -1,7 +0,0 @@
-const controlador = {
-    mostrarProductoAgregado: function(req,res){
-        return res.render('product-add')
-    },
-};
-
-module.exports = controlador;
--- miApp/routes/productAdd.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productAddController = require('../controllers/productAddController');
-
-router.get('/', productAddController.mostrarProductoAgregado);
-
-
-module.exports = router;
