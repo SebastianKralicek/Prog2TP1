e78927e0d72ab5f9ed835c62b8010ed3169a522d
biehay <bhaymesbiedma@udesa.edu.ar>
2025-04-14 09:41:53 -0300

    cambiado de error

--- miApp/routes/login.js
+++ miApp/routes/login.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.get('/', loginController.mostrarLogin)
-router.get('/procesoLogin', loginController.procesoLogin)
 
 module.exports = router;
