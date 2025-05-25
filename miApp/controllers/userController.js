77fe45155974162e0c6629afa73dbc83dca19039
Sebastian <skralicek@udesa.edu.ar>
2025-05-25 01:07:47 -0300

    login progreso

--- miApp/controllers/userController.js
+++ miApp/controllers/userController.js
@@ -1,4 +1,6 @@
 const data = require('../db/data')
+let db = require('../models')
+let bcrypt = require('bcryptjs')
 
 const controlador = {
     mostrarRegistro: function(req,res){
@@ -12,5 +14,29 @@ const controlador = {
         const productos = data.products; 
         res.render("profile", { usuario, productos }); 
     },
+    CrearRegistro: function(req, res){
+        let passEncriptada = bcrypt.hashSync(req.body.pass, 10)
+        let usuario = {
+            mail: req.body.mail,
+            username: req.body.userName,
+            DNI: req.body.dni,
+            pass: passEncriptada,
+            passretry: req.body.passretry
+        }
+        db.User.create(usuario)
+        .then(function(users){
+            return res.redirect('/login')
+        })
+        .catch(function(error){
+            return res.send(error)
+        })
+    },
+
+    ProcesoRegistro: function(req,res){
+        let EmailUsuario = req.body.mail;
+        let ContraUsuario = req.body.pass;
+
+        db.User
+    }
 };
 module.exports = controlador
