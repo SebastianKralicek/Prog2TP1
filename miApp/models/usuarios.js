7b079b9207fe84bb1bd0bb4616f3c7c5c92b98a3
biehay <bhaymesbiedma@udesa.edu.ar>
2025-05-26 10:22:40 -0300

    models users

---
+++ miApp/models/usuarios.js
@@ -0,0 +1,46 @@
+module.exports = function (sequelize, dataTypes){
+    let alias = 'User'; //Este alias se busca como nombre en de la tabla en plural dentro de la base de datos.
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        contrasenia: {
+            type: dataTypes.STRING
+        },
+        fecha_nacimiento:{
+            type: dataTypes.DATE
+        },
+        dni:{
+            type: dataTypes.INTEGER
+        },
+        foto_perfil: {
+            type: dataTypes.STRING
+        },
+         created_at : {
+            type: dataTypes.DATE,
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        delated_at: {
+            type: dataTypes.DATE,
+        },
+        
+    }
+
+    let config = {
+        tableName : "users",
+        timestamps:true,
+        underscored: true,
+    };
+
+    const User = sequelize.define(alias, cols, config);
+    
+    return User;
+}
