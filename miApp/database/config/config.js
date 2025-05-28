376a8e0f95e6532a767e1ca2a414963d65d642a8
biehay <bhaymesbiedma@udesa.edu.ar>
2025-05-28 15:38:00 -0300

    quite algo que agregue en config

--- miApp/database/config/config.js
+++ miApp/database/config/config.js
@@ -1,26 +1,23 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "",
     "database": "TrabajoPractico",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "8889" 
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "8889" 
+    "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "8889" 
+    "dialect": "mysql"
   }
 }
