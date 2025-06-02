409aac737e908ed338ce28a69323da293754fe49
SebastianKralicek <skralicek@udesa.edu.ar>
2025-06-02 00:30:37 -0300

    agregar producto se muestra

--- miApp/controllers/indexControllers.js
+++ miApp/controllers/indexControllers.js
@@ -1,11 +1,17 @@
 const data = require('../db/data')
+const db = require('../database/models')
+
 
 const controlador = {
-    TodosProds: function(req, res){
-        const productos = data.products;
-        res.render("index", {productos});
+    TodosProds: function(req, res) {
+        db.Product.findAll() 
+        .then(function(productos) {
+            res.render("index", { productos });
+        })
+        .catch(function(error) {
+            res.send("Error al cargar productos");
+        });
     }
-
-
 };
+
 module.exports = controlador;
